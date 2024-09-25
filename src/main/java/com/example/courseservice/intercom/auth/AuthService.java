f9b4d7ae1ddd025168c8f599e0ac4718a79dbba7
FloreDenis2001 <floredenis907@yahoo.com>
2024-09-25 22:04:46 +0300

    update

--- src/main/java/com/example/courseservice/intercom/auth/AuthService.java
+++ src/main/java/com/example/courseservice/intercom/auth/AuthService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 //@FeignClient(name = "auth-service", url = "http://localhost:8080/server/api/v1/")
-@FeignClient(name = "auth-service",url = "http://${param.courses-auth-service}:8080/server/api/v1/")
+@FeignClient(name = "auth-service",url = "http://${param.courses-auth-service}:8083/server/api/v1/")
 public interface AuthService {
     @GetMapping("/getUserRole")
     ResponseEntity<String> getUserRole(@RequestHeader("Authorization") String token);
